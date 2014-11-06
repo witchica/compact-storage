c0a867aef446e79de613a6c4603620adaa878256
Toby Strong <toby.strong1@hotmail.co.uk>
2014-11-06 22:29:57 +0000

    General: Added new information regarding containers to ChestType.

--- src/main/java/com/workshop/compactchests/block/ChestType.java
+++ src/main/java/com/workshop/compactchests/block/ChestType.java
@@ -9,13 +9,12 @@ import java.awt.*;
 /**
  * Created by Toby on 06/11/2014.
  */
-public enum ChestType
-{
-    DOUBLE(0, "double", 0x0066CC),
-    TRIPLE(1, "triple", 0x00CC00),
-    QUADRUPLE(2, "quadruple", 0xFF9900),
-    QUINTUPLE(3, "quintuple", 0xFF0000),
-    SEXTUPLE(4, "sextuple", 0xCC0099);
+public enum ChestType {
+    DOUBLE(0, "double", 0x0066CC, 9, 6),
+    TRIPLE(1, "triple", 0x00CC00, 9, 9),
+    QUADRUPLE(2, "quadruple", 0xFF9900, 12, 9),
+    QUINTUPLE(3, "quintuple", 0xFF0000, 15, 9),
+    SEXTUPLE(4, "sextuple", 0xCC0099, 18, 9);
 
     public int id;
     public String name;
@@ -25,8 +24,10 @@ public enum ChestType
     public int width;
     public int height;
 
-    ChestType(int id, String name, int defaultColor)
-    {
+    public int iconWidth;
+    public int iconHeight;
+
+    ChestType(int id, String name, int defaultColor, int width, int height) {
         this.id = id;
         this.name = name;
         this.defaultColor = defaultColor;
@@ -34,12 +35,22 @@ public enum ChestType
         this.gui = new ResourceLocation("compactstorage", "textures/gui/" + name + "Chest.png");
 
         ImageIcon icon = new ImageIcon(getClass().getResource("/assets/" + gui.getResourceDomain() + "/" + gui.getResourcePath()));
-        this.width = icon.getIconWidth();
-        this.height = icon.getIconHeight();
+        this.iconWidth = icon.getIconWidth();
+        this.iconHeight = icon.getIconHeight();
+
+        this.width = width;
+        this.height = height;
     }
 
-    public int getWidth()
-    {
+    public int getIconWidth() {
+        return iconWidth;
+    }
+
+    public int getIconHeight() {
+        return iconHeight;
+    }
+
+    public int getWidth() {
         return width;
     }
 
