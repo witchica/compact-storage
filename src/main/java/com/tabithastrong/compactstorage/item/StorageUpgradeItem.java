687fc9dc5164b78d918116d16fc18f1db0465f48
Tabitha Strong <tobymarkstrong@gmail.com>
2022-08-24 21:14:46 +0100

    Part way through update, closes #135

--- src/main/java/com/tabithastrong/compactstorage/item/StorageUpgradeItem.java
+++ src/main/java/com/tabithastrong/compactstorage/item/StorageUpgradeItem.java
@@ -12,18 +12,38 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 
 public class StorageUpgradeItem extends Item {
-    public StorageUpgradeItem(Properties settings) {
+    public enum StorageUpgradeLevel {
+        COPPER,
+        IRON,
+        GOLD,
+        DIAMOND,
+        EMERALD;
+    }
+
+    public enum StorageUpgradeDirection {
+        COLUMN,
+        ROW;
+    }
+
+    public StorageUpgradeLevel upgradeLevel;
+    public StorageUpgradeDirection upgradeDirection;
+
+    public StorageUpgradeItem(Properties settings, StorageUpgradeLevel level,StorageUpgradeDirection direction) {
         super(settings);
+        this.upgradeLevel = level;
+        this.upgradeDirection = direction;
     }
 
     @Override
     public void appendHoverText(ItemStack stack, @Nullable Level world, List<Component> tooltip, TooltipFlag context) {
         super.appendHoverText(stack, world, tooltip, context);
 
-        if(stack.getItem() == CompactStorage.UPGRADE_COLUMN_ITEM.get()) {
-            tooltip.add(Component.translatable("tooltip.compact_storage.column_upgrade_descriptor").withStyle(ChatFormatting.LIGHT_PURPLE));
-        } else if(stack.getItem() == CompactStorage.UPGRADE_ROW_ITEM.get()) {
-            tooltip.add(Component.translatable("tooltip.compact_storage.row_upgrade_descriptor").withStyle(ChatFormatting.LIGHT_PURPLE));
+        if(stack.getItem() instanceof StorageUpgradeItem storageUpgradeItem) {
+            if(storageUpgradeItem.upgradeDirection == StorageUpgradeDirection.COLUMN) {
+                tooltip.add(Component.translatable("tooltip.compact_storage.column_upgrade_descriptor").withStyle(ChatFormatting.LIGHT_PURPLE));
+            } else if(storageUpgradeItem.upgradeDirection == StorageUpgradeDirection.ROW) {
+                tooltip.add(Component.translatable("tooltip.compact_storage.row_upgrade_descriptor").withStyle(ChatFormatting.LIGHT_PURPLE));
+            }
         }
 
         tooltip.add(Component.translatable("tooltip.compact_storage.upgrade_backpack").withStyle(ChatFormatting.DARK_PURPLE));
