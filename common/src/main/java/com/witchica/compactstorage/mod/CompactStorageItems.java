914cb726aede09e8881994539dda25d3899cfb43
witchica <jessicastrongg@gmail.com>
2026-06-02 19:42:24 +0100

    Mod metadata

--- common/src/main/java/com/witchica/compactstorage/mod/CompactStorageItems.java
+++ common/src/main/java/com/witchica/compactstorage/mod/CompactStorageItems.java
@@ -70,6 +70,10 @@ public class CompactStorageItems {
                                     output.accept(CompactStorageBlocks.compactBarrels.get(storageType));
                                     output.accept(CompactStorageBlocks.itemDrums.get(storageType));
                                     output.accept(CompactStorageItems.BACKPACK_ITEMS.get(storageType));
+
+                                    output.accept(UPGRADE_WIDTH);
+                                    output.accept(UPGRADE_HEIGHT);
+                                    output.accept(UPGRADE_RETAINING);
                                 }
                             });
                         })
